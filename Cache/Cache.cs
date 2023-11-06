b11f7f49da5671693dd4b6cddfb254ac994cf4bf
6PaTeL <jkdjkd789@gmail.com>
2023-11-06 18:59:06 +0200

    Add project files.

---
+++ Cache/Cache.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cache
+{
+    public class Cache
+    {
+        private object[] _objects;
+        private int _size;
+        public int Count { get; set; } = 0;
+        
+        public Cache(int size)
+        {
+            _objects = new object[size];    
+            _size = size;   
+        }
+
+        public void SetData(int index, object data)
+        {
+            if (index < 0 || index >= _size)
+            {
+                throw new IndexOutOfRangeException(); 
+            }
+            _objects[index] = data;
+            Count++; 
+        }
+
+        public object GetData(int index)
+        {
+            if(index < 0 || index >= _size)
+            {
+                throw new IndexOutOfRangeException(); 
+            }
+            return _objects[index]; 
+        }
+    }
+}
---
+++ Cache/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cache
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Cache cache = new Cache(100);
+
+            int[] ints = { 1, 2, 3, 4, 5 };
+            char[] chars = { 'h', 'e', 'l', 'l', 'o' };
+
+            for (int i = cache.Count; i < ints.Length; i++)
+            {
+                cache.SetData(i, ints[i]);
+            }
+            for (int j = cache.Count; j < chars.Length; j++)
+            {
+                cache.SetData(j, chars[j]);
+            }
+        }
+    }
+}
